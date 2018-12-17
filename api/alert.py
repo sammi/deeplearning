a36f9fa7c57162b0c002929bda30374cd6186430
Sammi Song <songyunhui2008@gmail.com>
2018-12-16 17:31:18 -0800

    predict api return params.

--- api/alert.py
+++ api/alert.py
@@ -1,6 +1,17 @@
-from flask import Flask
+import pickle
+from flask import Flask, jsonify, Markup
+import numpy as np
+
 app = Flask(__name__)
 
 @app.route("/predict")
 def hello():
-    return "hi"
+    modelFileName = '../app/alert_params'
+
+    with open(modelFileName, 'rb') as input:
+        W, b = pickle.load(input)
+        print(W, b)
+        return jsonify({
+                    'W':np.array2string(W.asnumpy()),
+                    'b':np.array2string(b.asnumpy())
+                })
